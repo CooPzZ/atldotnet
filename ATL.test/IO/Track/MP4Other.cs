9cfba36be940764046cc562a617f357189c801fe
CooPzZ <pc.programming@hotmail.com>
2022-10-11 13:31:54 +1100

    Saving Meta over and over again bug?

--- ATL.test/IO/Track/MP4Other.cs
+++ ATL.test/IO/Track/MP4Other.cs
@@ -5,6 +5,7 @@ using ATL.Logging;
 using System.Collections.Generic;
 using ATL.Playlist;
 using Commons;
+using System;
 
 namespace ATL.test.IO.TrackObject
 
@@ -127,6 +128,207 @@ namespace ATL.test.IO.TrackObject
             Assert.IsTrue(dLenght > dPostLenght, "File should be smaller.");
             Assert.AreEqual(removeTag_expectedPostLenght, dPostLenght, $"File should be {removeTag_expectedPostLenght} once tags are removed - As per test CS_RemoveTag.");
         }
+
+
+        /// <summary>
+        /// Test Track.Remove() and editing still works.
+        /// </summary>
+        [TestMethod]
+        public void CS_RemoveTag_AddMeta()
+        {
+            //string fileToTestOn = audioFilePath;
+            System.IO.File.Delete(fileToTestOn);
+            System.IO.File.Copy("M:\\Temp\\Audio\\TestFromABC-Orig.m4b", fileToTestOn);
+            Track theFile = new Track(fileToTestOn);
+            double tDuration = theFile.DurationMs; System.Console.WriteLine("Pre Duration: " + tDuration);
+            double dLenght = FileLength(fileToTestOn); System.Console.WriteLine("Pre File Length: " + dLenght);
+            theFile.Remove(MetaDataIOFactory.TagType.NATIVE);
+            //theFile.Save(); //Dont need.
+            theFile = new Track(fileToTestOn);
+            double dPostLenght = FileLength(fileToTestOn);
+            System.Console.WriteLine("Clear Duration: " + theFile.DurationMs.ToString());
+            System.Console.WriteLine("Clear File Length: " + dPostLenght);
+
+            Assert.AreEqual(tDuration, theFile.DurationMs, "Duration should be the same.");
+            Assert.IsTrue(dLenght > dPostLenght, "File should be smaller.");
+            // 8 extra bytes because the empty padding atom (`free` atom) isn't removed by design when using Track.Remove
+            // as padding areas aren't considered as metadata per se, and are kept to facilitate file expansion
+            Assert.AreEqual(removeTag_expectedPostLenght + 8, dPostLenght, $"File should be {removeTag_expectedPostLenght + 8} once tags are removed.");
+
+            bool WithErrors = false;
+            
+            //Add Meta again
+            System.Console.WriteLine("Save 0: ");
+            var log = new ArrayLogger();
+            theFile = new Track(fileToTestOn);
+            theFile.Description = "New Description";
+            theFile.Title = "New Title";
+            theFile.Album = "New Album";
+            Action<float> progress = new Action<float>(x => System.Console.WriteLine(x.ToString()));
+            if (theFile.Save(progress) == false)
+                Assert.Fail("Failed to save.");
+            System.Console.WriteLine("ErrorLOG: ");
+            foreach (Logging.Log.LogItem l in log.GetAllItems(Logging.Log.LV_ERROR))
+                System.Console.WriteLine("- " + l.Message);
+            WithErrors = (WithErrors || log.GetAllItems(Logging.Log.LV_ERROR).Count > 0);
+
+            //Add Meta again
+            System.Console.WriteLine("Save 1: ");
+            log = new ArrayLogger();
+            theFile = new Track(fileToTestOn);
+            theFile.Description = "New Description1";
+            theFile.Title = "New Title1";
+            theFile.Album = "New Album1";
+            progress = new Action<float>(x => System.Console.WriteLine(x.ToString()));
+            if (theFile.Save(progress) == false)
+                Assert.Fail("Failed to save.");
+            System.Console.WriteLine("ErrorLOG: ");
+            foreach (Logging.Log.LogItem l in log.GetAllItems(Logging.Log.LV_ERROR))
+                System.Console.WriteLine("- " + l.Message);
+            WithErrors = (WithErrors || log.GetAllItems(Logging.Log.LV_ERROR).Count > 0);
+
+            //Add Meta again
+            System.Console.WriteLine("Save 2: ");
+            log = new ArrayLogger();
+            theFile = new Track(fileToTestOn);
+            theFile.Description = "New Description2";
+            theFile.Title = "New Title2";
+            theFile.Album = "New Album2";
+            progress = new Action<float>(x => System.Console.WriteLine(x.ToString()));
+            if (theFile.Save(progress) == false)
+                Assert.Fail("Failed to save.");
+            System.Console.WriteLine("ErrorLOG: ");
+            foreach (Logging.Log.LogItem l in log.GetAllItems(Logging.Log.LV_ERROR))
+                System.Console.WriteLine("- " + l.Message);
+            WithErrors = (WithErrors || log.GetAllItems(Logging.Log.LV_ERROR).Count > 0);
+
+            //Add Meta again
+            System.Console.WriteLine("Save 3: ");
+            log = new ArrayLogger();
+            theFile = new Track(fileToTestOn);
+            theFile.Description = "New Description3";
+            theFile.Title = "New Title3";
+            theFile.Album = "New Album3";
+            progress = new Action<float>(x => System.Console.WriteLine(x.ToString()));
+            if (theFile.Save(progress) == false)
+                Assert.Fail("Failed to save.");
+            System.Console.WriteLine("ErrorLOG: ");
+            foreach (Logging.Log.LogItem l in log.GetAllItems(Logging.Log.LV_ERROR))
+                System.Console.WriteLine("- " + l.Message);
+            WithErrors = (WithErrors || log.GetAllItems(Logging.Log.LV_ERROR).Count > 0);
+
+            theFile = new Track(fileToTestOn);
+            dPostLenght = FileLength(fileToTestOn);
+            System.Console.WriteLine("POST Add Duration: " + theFile.DurationMs.ToString());
+            System.Console.WriteLine("POST Add File Length: " + dPostLenght);
+            Assert.AreEqual("New Description3", theFile.Description, "Description should be the same.");
+            Assert.AreEqual("New Title3", theFile.Title, "Title should be the same.");
+            Assert.AreEqual("New Album3", theFile.Album, "Album should be the same.");
+
+
+            if (WithErrors) Assert.Fail("There were errors noted in the Logs on saving;");
+        }
+
+
+        /// <summary>
+        /// Test Track.Remove() and editing still works.
+        /// </summary>
+        [TestMethod]
+        public void CS_RemoveTag_AddMetaLoop()
+        {
+            //string fileToTestOn = audioFilePath;
+            System.IO.File.Delete(fileToTestOn);
+            System.IO.File.Copy("M:\\Temp\\Audio\\TestFromABC-Orig.m4b", fileToTestOn);
+            Track theFile = new Track(fileToTestOn);
+            double tDuration = theFile.DurationMs; System.Console.WriteLine("Pre Duration: " + tDuration);
+            double dLenght = FileLength(fileToTestOn); System.Console.WriteLine("Pre File Length: " + dLenght);
+            theFile.Remove(MetaDataIOFactory.TagType.ANY);
+            //theFile.Save(); //Dont need.
+            theFile = new Track(fileToTestOn);
+            double dPostLenght = FileLength(fileToTestOn);
+            System.Console.WriteLine("Clear Duration: " + theFile.DurationMs.ToString());
+            System.Console.WriteLine("Clear File Length: " + dPostLenght);
+
+            Assert.AreEqual(tDuration, theFile.DurationMs, "Duration should be the same.");
+            Assert.IsTrue(dLenght > dPostLenght, "File should be smaller.");
+            // 8 extra bytes because the empty padding atom (`free` atom) isn't removed by design when using Track.Remove
+            // as padding areas aren't considered as metadata per se, and are kept to facilitate file expansion
+            Assert.AreEqual(removeTag_expectedPostLenght + 8, dPostLenght, $"File should be {removeTag_expectedPostLenght + 8} once tags are removed.");
+
+            //Add Meta again
+            int TopEdit = 40; bool WithErrors = false;
+            for (int n = 0; n <= TopEdit; n++)
+            {
+                System.Console.WriteLine($"Save {n}: ");
+                var log = new ArrayLogger();
+                theFile = new Track(fileToTestOn);
+                theFile.Description = "New Description" + n.ToString();
+                theFile.Title = "New Title" + n.ToString();
+                theFile.Album = "New Album" + n.ToString();
+                Action<float> progress = new Action<float>(x => System.Console.WriteLine(x.ToString()));
+                if (theFile.Save(progress) == false)
+                    Assert.Fail("Failed to save.");
+                System.Console.WriteLine($"ErrorLOG: {n} ");
+                foreach (Logging.Log.LogItem l in log.GetAllItems(Logging.Log.LV_ERROR))
+                    System.Console.WriteLine("- " + l.Message);
+                WithErrors = (WithErrors || log.GetAllItems(Logging.Log.LV_ERROR).Count > 0);
+            }
+
+            theFile = new Track(fileToTestOn);
+            dPostLenght = FileLength(fileToTestOn);
+            System.Console.WriteLine("POST Add Duration: " + theFile.DurationMs.ToString());
+            System.Console.WriteLine("POST Add File Length: " + dPostLenght);
+            Assert.AreEqual($"New Description{TopEdit}", theFile.Description, "Description should be the same.");
+            Assert.AreEqual($"New Title{TopEdit}", theFile.Title, "Title should be the same.");
+            Assert.AreEqual($"New Album{TopEdit}", theFile.Album, "Album should be the same.");
+
+            if (WithErrors) Assert.Fail("There were errors noted in the Logs on saving;");
+        }
+
+
+        /// <summary>
+        /// Test Track.Remove() and editing still works.
+        /// </summary>
+        [TestMethod]
+        public void CS_AddMetaLoop()
+        {
+            //string fileToTestOn = audioFilePath;
+            System.IO.File.Delete(fileToTestOn);
+            System.IO.File.Copy("M:\\Temp\\Audio\\TestFromABC-Orig.m4b", fileToTestOn);
+            Track theFile = new Track(fileToTestOn);
+            double tDuration = theFile.DurationMs; System.Console.WriteLine("Pre Duration: " + tDuration);
+            double dLenght = FileLength(fileToTestOn); System.Console.WriteLine("Pre File Length: " + dLenght);
+
+            //Update Meta again
+            int TopEdit = 100; bool WithErrors = false;
+            for (int n = 0; n <= TopEdit; n++)
+            {
+                System.Console.WriteLine($"Save {n}: ");
+                var log = new ArrayLogger();
+                theFile = new Track(fileToTestOn);
+                theFile.Description = "New Description" + n.ToString();
+                theFile.Title = "New Title" + n.ToString();
+                theFile.Album = "New Album" + n.ToString();
+                Action<float> progress = new Action<float>(x => System.Console.WriteLine(x.ToString()));
+                if (theFile.Save(progress) == false)
+                    Assert.Fail("Failed to save.");
+                System.Console.WriteLine($"ErrorLOG: {n} ");
+                foreach (Logging.Log.LogItem l in log.GetAllItems(Logging.Log.LV_ERROR))
+                    System.Console.WriteLine("- " + l.Message);
+                WithErrors = (WithErrors || log.GetAllItems(Logging.Log.LV_ERROR).Count > 0);
+            }
+
+            theFile = new Track(fileToTestOn);
+            System.Console.WriteLine("POST Add Duration: " + theFile.DurationMs.ToString());
+            System.Console.WriteLine("POST Add File Length: " + FileLength(fileToTestOn));
+            Assert.AreEqual($"New Description{TopEdit}", theFile.Description, "Description should be the same.");
+            Assert.AreEqual($"New Title{TopEdit}", theFile.Title, "Title should be the same.");
+            Assert.AreEqual($"New Album{TopEdit}", theFile.Album, "Album should be the same.");
+
+            if (WithErrors) Assert.Fail("There were errors noted in the Logs on saving;");
+        }
+
+
 
         #endregion
 
